bd4ec9b952b11d5100af658c489a9adc0c81c680
GarimaUttam <garimauttam191203@gmail.com>
2025-03-09 15:42:11 +0530

    2nd method of protecting route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  "/", // Example: add public routes here
-  "/student",
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/teacher(.*)'])
 
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    return auth().then(authObject => {
-      if (!authObject.userId) {
-        return authObject.redirectToSignIn(); // Redirect to Clerk sign-in page
-      }
-    });
-  }
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
