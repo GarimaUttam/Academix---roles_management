ecc6da741e504240e9540ce944cf0ec83d11ba66
GarimaUttam <garimauttam191203@gmail.com>
2025-03-09 15:32:16 +0530

    Protecting the routes using middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,24 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/", // Example: add public routes here
+  "/student",
+
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    return auth().then(authObject => {
+      if (!authObject.userId) {
+        return authObject.redirectToSignIn(); // Redirect to Clerk sign-in page
+      }
+    });
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
